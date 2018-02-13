013d9850631bc7d2ed7d9cebf18f7970f499f028
nick <n.kravchenko@skins.services>
2018-02-13 18:49:59 +0200

    Was added such translates as: indonesian, japanese, portuguese, vietnamese. Also SEO texts was added.

---
+++ seo-texts.const.ts
@@ -0,0 +1,206 @@
+export const seoTexts = {
+  default: {
+    'main': {
+      title: 'SocialMedia.Marker soft cap reached during pre-ICO. BETA release in Feb ‘18',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'en'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Marker soft cap reached during pre-ICO. BETA release in Feb ‘18.'},
+        {
+          property: 'og:description',
+          content: 'The first decentralized platform connecting social media bloggers and advertisers. ' +
+                   'Winner of the ICOBOX contest. Top rated ICO by trackers.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'The first decentralized platform connecting social media bloggers and advertisers. ' +
+                   'Winner of the ICOBOX contest. Top rated ICO by trackers.'
+        },
+      ]
+    },
+  },
+  en: {
+    'main': {
+      title: 'SocialMedia.Marker soft cap reached during pre-ICO. BETA release in Feb ‘18',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'en'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Marker soft cap reached during pre-ICO. BETA release in Feb ‘18.'},
+        {
+          property: 'og:description',
+          content: 'The first decentralized platform connecting social media bloggers and advertisers. ' +
+          'Winner of the ICOBOX contest. Top rated ICO by trackers.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/en'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'The first decentralized platform connecting social media bloggers and advertisers. ' +
+          'Winner of the ICOBOX contest. Top rated ICO by trackers.'
+        },
+      ]
+    },
+  },
+  ru: {
+    'main': {
+      title: 'SocialMedia.Market достиг минимальной цели за время pre-ICO',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'ru'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market достиг минимальной цели за время pre-ICO.'},
+        {
+          property: 'og:description',
+          content: 'Первая децентрализованная платформа, объединяющая блогеров основных социальных сетей и рекламодателей. ' +
+                   'Победитель конкурса ICOBOX. Высокие рейтинги ICO у трекеров.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/ru'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'Первая децентрализованная платформа, объединяющая блогеров основных социальных сетей и рекламодателей. ' +
+                   'Победитель конкурса ICOBOX. Высокие рейтинги ICO у трекеров.'
+        },
+      ]
+    },
+  },
+  chi: {
+    'main': {
+      title: '了新一代网络广告 – SocialMedia.Market',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'zh'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market – 了新一代网络广告'},
+        {
+          property: 'og:description',
+          content: 'SocialMedia.Market – 第一个分散式的体系，专于研发、创造、运行和分析社会媒体影响者的广告活动.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/chi'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: '第一个分散式的体系，专于研发、创造、运行和分析社会媒体影响者的广告活动.'
+        },
+      ]
+    },
+  },
+  fr: {
+    'main': {
+      title: 'Next Generation of Online Advertising – SocialMedia.Market',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'en'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market – Next Generation of Online Advertising'},
+        {
+          property: 'og:description',
+          content: 'SocialMedia.Market – the first decentralized ecosystem to discover, create, ' +
+          'run and analyze advertising campaigns with social media influencers.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/en'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'The first decentralized ecosystem to discover, create, ' +
+          'run and analyze advertising campaigns with social media influencers.'
+        },
+      ]
+    },
+  },
+  de: {
+    'main': {
+      title: 'Neue Generation Der Internetwerbung – SocialMedia.Market',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'de'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market – Neue Generation Der Internetwerbung'},
+        {
+          property: 'og:description',
+          content: 'SocialMedia.Market – das erste dezentralisierte Ökosystem, um Werbekampagnen ' +
+                   'mit Influencern der sozialen Medien zu entdecken, zu schaffen, durchzuführen und zu analysieren.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/de'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'Das erste dezentralisierte Ökosystem, um Werbekampagnen mit Influencern der ' +
+                   'sozialen Medien zu entdecken, zu schaffen, durchzuführen und zu analysieren.'
+        },
+      ]
+    },
+  },
+  es: {
+    'main': {
+      title: 'Nueva Generación de Publicidad en Línea – SocialMedia.Market',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'es'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market – Nueva Generación de Publicidad en Línea'},
+        {
+          property: 'og:description',
+          content: 'SocialMedia.Market – el primer ecosistema descentralizado para descubrir, crear, ' +
+                   'ejecutar y analizar campañas de publicidad con personas de influencia en medios sociales.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/es'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'El primer ecosistema descentralizado para descubrir, crear, ejecutar y ' +
+                   'analizar campañas de publicidad con personas de influencia en medios sociales.'
+        },
+      ]
+    },
+  },
+  ko: {
+    'main': {
+      title: '차세대 온라인 광고 플랫폼을 – SocialMedia.Market',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'ko'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market – 차세대 온라인 광고 플랫폼을'},
+        {
+          property: 'og:description',
+          content: 'SocialMedia.Market – 는 소셜 미디어 상의 인플로언서(영향력 있는 개인)를 통해 광고 캠페인을 발견, 창조, 운영 및 분석하는 최초로 분권화된 생태계를 표방하고 있습니다.  '
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/ko'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: '는 소셜 미디어 상의 인플로언서(영향력 있는 개인)를 통해 광고 캠페인을 발견, 창조, 운영 및 분석하는 최초로 분권화된 생태계를 표방하고 있습니다.'
+        },
+      ]
+    },
+  },
+};
