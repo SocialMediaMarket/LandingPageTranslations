87243b7e6627620ad91a076087493d973adb2a03
tanyasapa <36409537+tanyasapa@users.noreply.github.com>
2018-02-14 14:32:23 +0200

    added new languages

--- seo-texts.const.ts
+++ seo-texts.const.ts
@@ -53,12 +53,12 @@ export const seoTexts = {
   },
   ru: {
     'main': {
-      title: 'SocialMedia.Market достиг минимальной цели за время pre-ICO',
+      title: 'SocialMedia.Market - ICO уже идет. Не пропусти!',
       metaTags: [
         {property: 'og:type', content: 'website'},
         {property: 'og:locale', content: 'ru'},
         {property: 'og:type', content: 'website'},
-        {property: 'og:title', content: 'SocialMedia.Market достиг минимальной цели за время pre-ICO.'},
+        {property: 'og:title', content: 'SocialMedia.Market - ICO уже идет. Не пропусти!'},
         {
           property: 'og:description',
           content: 'Первая децентрализованная платформа, объединяющая блогеров основных социальных сетей и рекламодателей. ' +
@@ -79,15 +79,15 @@ export const seoTexts = {
   },
   chi: {
     'main': {
-      title: '了新一代网络广告 – SocialMedia.Market',
+      title: 'SocialMedia.Market - 已达到软顶！代币销售正在进行中！',
       metaTags: [
         {property: 'og:type', content: 'website'},
         {property: 'og:locale', content: 'zh'},
         {property: 'og:type', content: 'website'},
-        {property: 'og:title', content: 'SocialMedia.Market – 了新一代网络广告'},
+        {property: 'og:title', content: 'SocialMedia.Market - 已达到软顶！代币销售正在进行中！'},
         {
           property: 'og:description',
-          content: 'SocialMedia.Market – 第一个分散式的体系，专于研发、创造、运行和分析社会媒体影响者的广告活动.'
+          content: '首个连接社交媒体，博主和广告商的去中心化市场。 解决主要的影响者营销问题，并为内容货币化、社区参与视听者接触创造机会。'
         },
         {property: 'og:url', content: 'https://socialmedia.market/chi'},
         {property: 'og:site_name', content: 'SocialMedia.Market'},
@@ -96,49 +96,52 @@ export const seoTexts = {
         {property: 'og:image:height', content: '1200'},
         {
           property: 'description',
-          content: '第一个分散式的体系，专于研发、创造、运行和分析社会媒体影响者的广告活动.'
+          content: '首个连接社交媒体，博主和广告商的去中心化市场。 解决主要的影响者营销问题，并为内容货币化、社区参与视听者接触创造机会。'
         },
       ]
     },
   },
   fr: {
     'main': {
-      title: 'Next Generation of Online Advertising – SocialMedia.Market',
+      title: 'SocialMedia.Market - SOFT CAP ATTEINT! Vente de token EN COURS!',
       metaTags: [
         {property: 'og:type', content: 'website'},
-        {property: 'og:locale', content: 'en'},
+        {property: 'og:locale', content: 'fr'},
         {property: 'og:type', content: 'website'},
-        {property: 'og:title', content: 'SocialMedia.Market – Next Generation of Online Advertising'},
+        {property: 'og:title', content: 'SocialMedia.Market - SOFT CAP ATTEINT! Vente de token EN COURS!'},
         {
           property: 'og:description',
-          content: 'SocialMedia.Market – the first decentralized ecosystem to discover, create, ' +
-          'run and analyze advertising campaigns with social media influencers.'
+          content: 'La premiére place de marché décentralisé reliant les blogueurs sur les médias sociaux et les annonceurs. ' +
+          'Résoudre les problèmes majeurs en marketing des influenceurs, SocialMedia.Market créera de nouvelles ' +
+          'opportunités pour la monétisation du contenu, l\'engagement communautaire et l\'exposition publique.'
         },
-        {property: 'og:url', content: 'https://socialmedia.market/en'},
+        {property: 'og:url', content: 'https://socialmedia.market/fr'},
         {property: 'og:site_name', content: 'SocialMedia.Market'},
         {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
         {property: 'og:image:width', content: '1200'},
         {property: 'og:image:height', content: '1200'},
         {
           property: 'description',
-          content: 'The first decentralized ecosystem to discover, create, ' +
-          'run and analyze advertising campaigns with social media influencers.'
+          content: 'La premiére place de marché décentralisé reliant les blogueurs sur les médias sociaux et les annonceurs. ' +
+          'Résoudre les problèmes majeurs en marketing des influenceurs, SocialMedia.Market créera de nouvelles ' +
+          'opportunités pour la monétisation du contenu, l\'engagement communautaire et l\'exposition publique.'
         },
       ]
     },
   },
   de: {
     'main': {
-      title: 'Neue Generation Der Internetwerbung – SocialMedia.Market',
+      title: 'SocialMedia.Market - SOFT CAP ERREICHT! Token Verkauf LIVE!',
       metaTags: [
         {property: 'og:type', content: 'website'},
         {property: 'og:locale', content: 'de'},
         {property: 'og:type', content: 'website'},
-        {property: 'og:title', content: 'SocialMedia.Market – Neue Generation Der Internetwerbung'},
+        {property: 'og:title', content: 'SocialMedia.Market - SOFT CAP ERREICHT! Token Verkauf LIVE!'},
         {
           property: 'og:description',
-          content: 'SocialMedia.Market – das erste dezentralisierte Ökosystem, um Werbekampagnen ' +
-                   'mit Influencern der sozialen Medien zu entdecken, zu schaffen, durchzuführen und zu analysieren.'
+          content: 'Der erste dezentralisierte Marktplatz verbindet Social-Media-Blogger und Werbetreibende. ' +
+                   'Lösung von Marketingproblemen von großen Influencer SocialMedia.Market ist die neue Möglichkeiten ' +
+                   'für die Monetarisierung von Inhalten, das Engagement von Communities und die Präsenz von Zielgruppen schaffen.'
         },
         {property: 'og:url', content: 'https://socialmedia.market/de'},
         {property: 'og:site_name', content: 'SocialMedia.Market'},
@@ -147,24 +150,26 @@ export const seoTexts = {
         {property: 'og:image:height', content: '1200'},
         {
           property: 'description',
-          content: 'Das erste dezentralisierte Ökosystem, um Werbekampagnen mit Influencern der ' +
-                   'sozialen Medien zu entdecken, zu schaffen, durchzuführen und zu analysieren.'
+          content: 'Der erste dezentralisierte Marktplatz verbindet Social-Media-Blogger und Werbetreibende. ' +
+                   'Lösung von Marketingproblemen von großen Influencer SocialMedia.Market ist die neue Möglichkeiten ' +
+                   'für die Monetarisierung von Inhalten, das Engagement von Communities und die Präsenz von Zielgruppen schaffen.'
         },
       ]
     },
   },
   es: {
     'main': {
-      title: 'Nueva Generación de Publicidad en Línea – SocialMedia.Market',
+      title: 'SocialMedia.Market - CAP MINIMO ALCANZADO! Venta de Tokens ACTIVA!',
       metaTags: [
         {property: 'og:type', content: 'website'},
         {property: 'og:locale', content: 'es'},
         {property: 'og:type', content: 'website'},
-        {property: 'og:title', content: 'SocialMedia.Market – Nueva Generación de Publicidad en Línea'},
+        {property: 'og:title', content: 'SocialMedia.Market - CAP MINIMO ALCANZADO! Venta de Tokens ACTIVA!'},
         {
           property: 'og:description',
-          content: 'SocialMedia.Market – el primer ecosistema descentralizado para descubrir, crear, ' +
-                   'ejecutar y analizar campañas de publicidad con personas de influencia en medios sociales.'
+          content: 'El primer mercado descentralizado conectando medios sociales, bloggers y publicistas. ' +
+                   'Solventando los mayores problemas de publicidad SocialMedia.Market creará nuevas oportunidades ' +
+                   'para monetizar su contenido, la interacción de la comunidad y la expocisión de audiencias.'
         },
         {property: 'og:url', content: 'https://socialmedia.market/es'},
         {property: 'og:site_name', content: 'SocialMedia.Market'},
@@ -173,23 +178,25 @@ export const seoTexts = {
         {property: 'og:image:height', content: '1200'},
         {
           property: 'description',
-          content: 'El primer ecosistema descentralizado para descubrir, crear, ejecutar y ' +
-                   'analizar campañas de publicidad con personas de influencia en medios sociales.'
+          content: 'El primer mercado descentralizado conectando medios sociales, bloggers y publicistas. ' +
+                   'Solventando los mayores problemas de publicidad SocialMedia.Market creará nuevas oportunidades ' +
+                   'para monetizar su contenido, la interacción de la comunidad y la expocisión de audiencias.'
         },
       ]
     },
   },
   ko: {
     'main': {
-      title: '차세대 온라인 광고 플랫폼을 – SocialMedia.Market',
+      title: 'SocialMedia.Market - 소프트캡 달성! 토큰 세일 라이브!',
       metaTags: [
         {property: 'og:type', content: 'website'},
         {property: 'og:locale', content: 'ko'},
         {property: 'og:type', content: 'website'},
-        {property: 'og:title', content: 'SocialMedia.Market – 차세대 온라인 광고 플랫폼을'},
+        {property: 'og:title', content: 'SocialMedia.Market - 소프트캡 달성! 토큰 세일 라이브!'},
         {
           property: 'og:description',
-          content: 'SocialMedia.Market – 는 소셜 미디어 상의 인플로언서(영향력 있는 개인)를 통해 광고 캠페인을 발견, 창조, 운영 및 분석하는 최초로 분권화된 생태계를 표방하고 있습니다.  '
+          content: '소셜 미디어 블로거와 광고주를 연결하는 최초의 탈중앙화 시장. 영향을 미치는 주요 마케팅 문제 해결 ' +
+                   'SocialMedia.Market은 콘텐츠의 수익 창출, 커뮤니티 참여 및 구독자에 대한 노출을 위한 새로운 기회를 창조합니다. ' 
         },
         {property: 'og:url', content: 'https://socialmedia.market/ko'},
         {property: 'og:site_name', content: 'SocialMedia.Market'},
@@ -198,7 +205,118 @@ export const seoTexts = {
         {property: 'og:image:height', content: '1200'},
         {
           property: 'description',
-          content: '는 소셜 미디어 상의 인플로언서(영향력 있는 개인)를 통해 광고 캠페인을 발견, 창조, 운영 및 분석하는 최초로 분권화된 생태계를 표방하고 있습니다.'
+          content: '소셜 미디어 블로거와 광고주를 연결하는 최초의 탈중앙화 시장. 영향을 미치는 주요 마케팅 문제 해결 ' +
+                   'SocialMedia.Market은 콘텐츠의 수익 창출, 커뮤니티 참여 및 구독자에 대한 노출을 위한 새로운 기회를 창조합니다. '
+        },
+      ]
+    },
+  },
+  vi: {
+    'main': {
+      title: 'SocialMedia.Market - GIỚI HẠN MỀM ĐÃ ĐẠT! Bán Token đang hoạt động!',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'vi'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market - GIỚI HẠN MỀM ĐÃ ĐẠT! Bán Token đang hoạt động!'},
+        {
+          property: 'og:description',
+          content: 'Sàn thương mại phân cấp đầu tiên kết nối các bloggers mạng xã hội với các nhà quảng cáo. ' +
+                   'Giải quyết các vấn đề chính về marketing sửu dụng người có tầm ảnh hưởng, SocialMedia.Market ' +
+                   'sẽ tạo ra những cơ hội mới cho việc tiền tệ hóa nội dung, gắn kết cộng đồng và tiếp cận khán giả.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/vi'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'Sàn thương mại phân cấp đầu tiên kết nối các bloggers mạng xã hội với các nhà quảng cáo. ' +
+                   'Giải quyết các vấn đề chính về marketing sửu dụng người có tầm ảnh hưởng, SocialMedia.Market ' +
+                   'sẽ tạo ra những cơ hội mới cho việc tiền tệ hóa nội dung, gắn kết cộng đồng và tiếp cận khán giả.'
+        },
+      ]
+    },
+  },
+  ja: {
+    'main': {
+      title: 'SocialMedia.Market - ソフトキャップが届きました！ トークンセールライブ！',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'ja'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market - ソフトキャップが届きました！ トークンセールライブ！'},
+        {
+          property: 'og:description',
+          content: 'インフルエンサーと広告を繋ぐ初めての分散型マーケット。SocialMedia.Marketはマーケティングの ' +
+                   '主要な問題を解決し、コンテンツ収益化、コミュニティエンゲージメントなどの新しい機会を作り出します。'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/ja'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'インフルエンサーと広告を繋ぐ初めての分散型マーケット。SocialMedia.Marketはマーケティングの ' +
+                   '主要な問題を解決し、コンテンツ収益化、コミュニティエンゲージメントなどの新しい機会を作り出します。'
+        },
+      ]
+    },
+  },
+  pt: {
+    'main': {
+      title: 'SocialMedia.Market - SOFT CAP ALCANÇADO! A Venda de Token Ativa!',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'pt'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market - SOFT CAP ALCANÇADO! A Venda de Token Ativa!'},
+        {
+          property: 'og:description',
+          content: 'O primeiro mercado descentralizado conectado de blogueiros de redes sociais e anunciantes. ' +
+                   'Resolvendo os principais problemas do marketing de influenciadores,  SocialMedia.Market criará novas ' +
+                   'oportunidades para a monetização de conteúdo, o envolvimento da comunidade e a exposição do público.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/pt'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'O primeiro mercado descentralizado conectado de blogueiros de redes sociais e anunciantes. ' +
+                   'Resolvendo os principais problemas do marketing de influenciadores,  SocialMedia.Market criará novas ' +
+                   'oportunidades para a monetização de conteúdo, o envolvimento da comunidade e a exposição do público.'
+        },
+      ]
+    },
+  },
+  id: {
+    'main': {
+      title: 'SocialMedia.Market - SOFT CAP TERCAPAI! Penjualan token sedang berlangsung!',
+      metaTags: [
+        {property: 'og:type', content: 'website'},
+        {property: 'og:locale', content: 'id'},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:title', content: 'SocialMedia.Market - SOFT CAP TERCAPAI! Penjualan token sedang berlangsung!'},
+        {
+          property: 'og:description',
+          content: 'Marketplace pertama terdesentralisasi yang menghubungkan blogger sosial media dan pengiklan. ' +
+                   'Memecahkan masalah pemasaran influencer utama SocialMedia.Market akan menciptakan  ' +
+                   'peluang baru untuk konten monetisasi, keterlibatan masyarakat dan penghargaan penonton.'
+        },
+        {property: 'og:url', content: 'https://socialmedia.market/id'},
+        {property: 'og:site_name', content: 'SocialMedia.Market'},
+        {property: 'og:image', content: 'https://socialmedia.market/assets/img/smm_preview.jpg'},
+        {property: 'og:image:width', content: '1200'},
+        {property: 'og:image:height', content: '1200'},
+        {
+          property: 'description',
+          content: 'Marketplace pertama terdesentralisasi yang menghubungkan blogger sosial media dan pengiklan. ' +
+                   'Memecahkan masalah pemasaran influencer utama SocialMedia.Market akan menciptakan  ' +
+                   'peluang baru untuk konten monetisasi, keterlibatan masyarakat dan penghargaan penonton.'
         },
       ]
     },
