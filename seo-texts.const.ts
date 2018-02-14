12c8e8960a48f04604a38d888de1261143f91d57
tanyasapa <36409537+tanyasapa@users.noreply.github.com>
2018-02-14 16:25:32 +0200

    some phrases updated

--- seo-texts.const.ts
+++ seo-texts.const.ts
@@ -1,12 +1,12 @@
 export const seoTexts = {
   default: {
     'main': {
-      title: 'SocialMedia.Market - Soft Cap REACHED! Token Sale LIVE!',
+      title: 'SocialMedia.Market - Soft Cap REACHED! ICO is LIVE!',
       metaTags: [
         {property: 'og:type', content: 'website'},
         {property: 'og:locale', content: 'en'},
         {property: 'og:type', content: 'website'},
-        {property: 'og:title', content: 'SocialMedia.Market - Soft Cap REACHED! Token Sale LIVE!'},
+        {property: 'og:title', content: 'SocialMedia.Market - Soft Cap REACHED! ICO is LIVE!'},
         {
           property: 'og:description',
           content: 'The first decentralized platform connecting social media bloggers and advertisers. ' +
@@ -27,12 +27,12 @@ export const seoTexts = {
   },
   en: {
     'main': {
-      title: 'SocialMedia.Market - Soft Cap REACHED! Token Sale LIVE!',
+      title: 'SocialMedia.Market - Soft Cap REACHED! ICO is LIVE!',
       metaTags: [
         {property: 'og:type', content: 'website'},
         {property: 'og:locale', content: 'en'},
         {property: 'og:type', content: 'website'},
-        {property: 'og:title', content: 'SocialMedia.Market - Soft Cap REACHED! Token Sale LIVE!'},
+        {property: 'og:title', content: 'SocialMedia.Market - Soft Cap REACHED! ICO is LIVE!'},
         {
           property: 'og:description',
           content: 'The first decentralized platform connecting social media bloggers and advertisers. ' +
